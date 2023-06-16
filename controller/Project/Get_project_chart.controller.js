39d1b14057a8bef1103f20f9e800258b3889a005
arjunsen1999 <arjunsen717@gmail.com>
2023-06-16 21:59:20 +0530

    update chart status controller

--- controller/Project/Get_project_chart.controller.js
+++ controller/Project/Get_project_chart.controller.js
@@ -1,62 +1,56 @@
 const { Project_model } = require("../../models/Project.model");
 
+const createlabels = (name) =>{
+ if(name == "Startegy"){
+  return "STR";
+ }else if(name === "Finance"){
+  return "FIN";
+ }else if(name === "Quality"){
+  return "QLT";
+ }else if(name === "Maintenance"){
+  return "MAN";
+ }else if(name === "Stores"){
+  return "STO";
+ }else if(name === "HR"){
+  return "HR"
+ }
+}
+
 const Get_project_chart_controller = async (req, res) => {
   try {
-    const total_str = await Project_model.find({ dept: "Startegy" }).count();
-    const close_str = await Project_model.find({
-      dept: "Startegy",
-      status: "Closed",
-    }).count();
+    const pipeline = [
+      {
+        $group: {
+          _id: "$dept",
+          total: { $sum: 1 },
+          closed: {
+            $sum: {
+              $cond: [{ $eq: ["$status", "Closed"] }, 1, 0],
+            },
+          },
+        },
+      },
+    ];
 
-    const total_fin = await Project_model.find({ dept: "Finance" }).count();
-    const close_fin = await Project_model.find({
-      dept: "Finance",
-      status: "Closed",
-    }).count();
+    const results = await Project_model.aggregate(pipeline);
 
-    const total_qlt = await Project_model.find({ dept: "Quality" }).count();
-    const close_qlt = await Project_model.find({
-      dept: "Quality",
-      status: "Closed",
-    }).count();
+    const labels = [];
+    const total_project = [];
+    const total_close = [];
 
-    const total_man = await Project_model.find({ dept: "Maintenance" }).count();
-    const close_man = await Project_model.find({
-      dept: "Maintenance",
-      status: "Closed",
-    }).count();
-
-    const total_sto = await Project_model.find({ dept: "Stores" }).count();
-    const close_sto = await Project_model.find({
-      dept: "Stores",
-      status: "Closed",
-    }).count();
-
-    const total_hr = await Project_model.find({ dept: "HR" }).count();
-    const close_hr = await Project_model.find({
-      dept: "HR",
-      status: "Closed",
-    }).count();
+    results.forEach((result) => {
+      const { _id, total, closed } = result;
+      const name = createlabels(_id)
+      labels.push(name);
+      total_project.push(total);
+      total_close.push(closed)
+    });
 
     const response = {
       status: "success",
-      labels: ["STR", "FIN", "QLT", "MAN", "STO", "HR"],
-      total_project: [
-        total_str,
-        total_fin,
-        total_qlt,
-        total_man,
-        total_sto,
-        total_hr,
-      ],
-      total_close: [
-        close_str,
-        close_fin,
-        close_qlt,
-        close_man,
-        close_sto,
-        close_hr,
-      ],
+      labels,
+      total_project,
+      total_close,
     };
     return res.status(201).json(response);
   } catch (error) {
